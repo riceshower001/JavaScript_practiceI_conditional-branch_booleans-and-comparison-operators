7767aab093b1d0318146a880b33b46630db4ccf4
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-02-25 22:36:17 +0900

    Add files via upload

---
+++ JavaScript_practiceI_conditional branch_booleans and comparison operators.js
@@ -0,0 +1,12 @@
+const age = 24;
+
+// Please output "age >= 20".
+console.log(age >= 20) ;
+
+// Please output "age < 20".
+console.log(age < 20) ;
+
+// If the value of age is 20 or more, print "I am over 20.".
+if(age >= 20){
+  console.log("I am over 20.") ;
+}
